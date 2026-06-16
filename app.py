018acd5d1817cb10c7ded6359793c7c04a22475f
zephirlorenzo-sudo <zephir.lorenzo@gmail.com>
2026-06-16 11:21:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Tiêu đề ứng dụng
-st.title("💰 Ứng dụng tính tiền gửi tiết kiệm")
+st.title("💰 Ứng dụng tính tiền gửi tiết kiệm_trucmy")
 
 # Nhập dữ liệu
 C = st.number_input(
